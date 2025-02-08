dea02c03962590abe2d334ebaaf9137a8e0a331e
Alexis Menendez <alexis.menendez@austincc.edu>
2025-02-08 14:21:48 -0600

    license badge
    Created a function that returns a license badge based on which license is passed in

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// TODO: Create a function that returns a license badge based on which license is passed in, if there is no license, return an empty string
+
+// NOTE TO SELF (add into notes later, remove this comment before final submission of project):
+// the URL format for licence badges is: https://img.shields.io/badge/{label}-{message}-{color}.svg
+
+function renderLicenseBadge(license) {
+  if (license === "None") {
+    return "";
+  }
+  return `![License](https://img.shields.io/badge/license-${license.replace(" ", "%20")}-blue.svg)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
